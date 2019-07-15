860cd35a6545180f9041c7614b073493ef329316
nihaljames <52252893+nihaljames@users.noreply.github.com>
2019-07-15 10:18:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-from flask import Flask, render_template      
-
+from flask import Flask, render_template
 import requests
-
-url = 'https://api.chucknorris.io/jokes/random'
-
-response = requests.get(url).text
-
-start = response.index('value":') + 7
-end = response.index('}')
-
-quote = response[start:end]
-file = open('static/css/test2.txt','w')
-file.write(quote)
-file.close()
-
-
+  
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,14 +10,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")    
-    
+
 @app.route("/nihal")
 def nihal():
     return "Hello, Nihal"
-    
+
 @app.route("/norris")
 def norris():
     return render_template("dailyquotes.html")
+
+@app.route("/newquote")
+def newquote():
+    url = 'https://api.chucknorris.io/jokes/random'
+    response = requests.get(url).text
+    return response
     
 if __name__ == "__main__":
     app.run(debug=True) 
+
